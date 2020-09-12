be99945d33fb0035372ae695e2fcabb4067297fa
AMRⓗⓐⓧⓩ404 <amrin10122000@gmail.com>
2020-09-12 13:39:35 +0700

    Instance Object

--- index.php
+++ index.php
@@ -21,4 +21,20 @@ $bmw->tipe = "320i";
 $bmw->mesin = "2000cc";
 $bmw->max_speed = "280km/h";
 
-echo "Tipe " . $bmw->kecepatanMaksimal();
+$bmw2 = new Mobil;
+$bmw2->merk = "BMW";
+$bmw2->tipe = "320i";
+$bmw2->mesin = "2000cc";
+$bmw2->max_speed = "280km/h";
+
+
+
+echo "$bmw->tipe";
+echo "<br>";
+echo "$bmw2->tipe";
+
+if ($bmw === $bmw2) {
+    echo "Sama";
+} else {
+    echo "Tidak";
+}
